141c53ffe0a8c581f10505859033a1cb1765dc49
sharon smith <sharon.e.smith2@gmail.com>
2017-02-01 22:40:44 -0600

    working on event listeners / handlers for radio buttons ; added temp conversion functions

--- converter.js
+++ converter.js
@@ -15,22 +15,39 @@ function checkForEnter(thisChar) {
 var userInput = checkForEnter(thisChar); // if <Enter>, returns user temp
 console.log(userInput);
 
-function toCelsius () {
+function clearInputField() {
+	document.getElementById("temp").innerHTML = "";
+}
 
+
+
+function toCelsius () {
+F => C :: [°C] = ([°F] − 32) ×  5⁄9
+	var fTemp = document.getElementById("temp").innerHTML;
+	return [ ( fTemp - 32 ) * ( 5/9 ) ];
 }
 
 function toFahrenheit () {
-
+// C => F :: [°F] = [°C] ×  9⁄5 + 32
+	var cTemp = document.getElementById("temp").innerHTML;
+	return [ cTemp * ( 9/5 ) ] + 32; 
 }
 
 // Get a reference to the button element in the DOM
-// var button = document.getElementById("converter");
+var rButtons = document.getElementById("converter"); // where is this needed/used???
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
   console.log("event", clickEvent);
+  var whichConv = document.getElementById("converter").value;
+  console.log(whichConv);
+  if (whichConv === "cels") {
+  	toFahrenheit();
+  } else {
+  	toCelsius();
+  }
 }
 
 // Assign a function to be executed when the button is clicked
-// button.addEventListener("click", determineConverter);
+button.addEventListener("click", determineConverter);
