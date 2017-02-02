4a9a36744c20f158b707a3dd0ce52fc1740fa861
sharon smith <sharon.e.smith2@gmail.com>
2017-02-01 19:55:23 -0600

    commiting code <so far> on handling onkeyup event / stuck

--- converter.js
+++ converter.js
@@ -1,5 +1,19 @@
 console.log("converter is running");
 
+function checkForEnter(thisChar) {
+	var enterKey = 13;
+	if (thisChar === enterKey) { // if (thisChar.which === enterKey)
+		console.log("reading temp input")
+		return document.getElementById("temp").innerHTML; // capture user input temp value
+	}
+}
+
+// function readInput() {
+// 	document.getElementById("temp").innerHTML; // capture user input temp value
+// }
+
+var userInput = checkForEnter(thisChar); // if <Enter>, returns user temp
+console.log(userInput);
 
 function toCelsius () {
 
@@ -10,7 +24,7 @@ function toFahrenheit () {
 }
 
 // Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
+// var button = document.getElementById("converter");
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
@@ -19,4 +33,4 @@ function determineConverter (clickEvent) {
 }
 
 // Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+// button.addEventListener("click", determineConverter);
