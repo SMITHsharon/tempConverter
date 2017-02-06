3aaaa39b5314fc7dc50f4f7b3d5a61caf3d5ba08
sharon smith <sharon.e.smith2@gmail.com>
2017-02-05 21:34:18 -0600

    event handlers working; working on functionality

--- converter.js
+++ converter.js
@@ -1,30 +1,91 @@
 console.log("converter is running");
 
-window.addEventListener('keyup', 
+// define Event Listners
+var converterButton = document.getElementById("converter");
+var clearButton = document.getElementById("clear");
+var enterKey = document.getElementById("temp");
+
+enterKey.addEventListener("keyup", 
 	function (e) {
-		console.log("listening for Enter Key");
 		if (e.keyCode === 13) {
-			getUserInput();
- 		}, false ); 		
-}
-// function checkForEnter(thisChar) {
-// 	var enterKey = 13;
-// 	if (thisChar === enterKey) { // if (thisChar.which === enterKey)
-// 		console.log("reading temp input")
-// 		return document.getElementById("temp").innerHTML; // capture user input temp value
-// 	}
-// }
+			// console.log("pressed Enter Key");
+			performConversion ();
+ 		} else {
+			// console.log("pressed something else");
+ 			return false;
+ 		} 		
+});
 
+converterButton.addEventListener("click", function() {
+	performConversion ();
+});
 
-// this won't work as the needed value is not being stored in a var ... being called from event handler?
-function getUserInput() 
-	return document.getElementById("temp").innerHTML; // capture user input temp value
-}
-
+clearButton.addEventListener("click", function(){
+  // console.log("clearButton working");
+  clearInputField()''
+});
 
 function clearInputField() {
 	document.getElementById("temp").innerHTML = "";
 }
+
+
+
+//*******************************************
+// if <Enter> key or <Convert> button, 
+// determine which conversion to calculate
+// function determineConverter (clickEvent) {
+function determineConverter (whichConv) {
+  // console.log("determining converter");
+  var whichConv;
+  // console.log("whichConv");
+  if (document.getElementById("cels").checked) {
+  	whichConv = "toCels";
+  } else if (document.getElementById("fahr").checked) {
+  	whichConv = "toFahr";
+    } else {
+        alert("Please select either Fahrenheit or Celsius for conversion.");
+    }
+}
+
+//*******************************************
+// Calculate the temperature conversion
+function calcConversion (CorF) {
+  // console.log("calculating converter");
+	// var cTemp = document.getElementById("temp").innerHTML;
+	// var fTemp = document.getElementById("temp").innerHTML;
+	inputTemp = document.getElementById("temp").value;
+	if (CorF === "toFahr") {
+		// C => F :: [°F] = [°C] ×  9⁄5 + 32
+		return [ inputTemp * ( 9/5 ) ] + 32; 
+	} else { // "toCels"
+		// F => C :: [°C] = ([°F] − 32) ×  5⁄9
+		return [ ( inputTemp - 32 ) * ( 5/9 ) ];
+	}
+}
+	
+
+
+  
+	
+
+
+function performConversion () {
+	var whichConv;
+	var convertedTemp;
+	whichConv = determineConverter(whichConv);
+	convertedTemp = calcConversion(whichConv);
+
+}
+
+
+// // this won't work as the needed value is not being stored in a var ... being called from event handler?
+// function getUserInput() {
+// 	return document.getElementById("temp").innerHTML; // capture user input temp value
+// }
+
+
+
 
 
 
@@ -43,20 +104,9 @@ function toFahrenheit () {
 // Get a reference to the button element in the DOM
 var rButtons = document.getElementById("converter"); // where is this needed/used???
 
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-function determineConverter (clickEvent) {
-  console.log("event", clickEvent);
-  var whichConv = document.getElementById("converter").value;
-  console.log(whichConv);
-  if (whichConv === "cels") {
-  	toFahrenheit();
-  } else {
-  	toCelsius();
-  }
-}
+
 
 // Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+converterButton.addEventListener("click", determineConverter);
 
 
