2397197a839547b112e9804b3d720554e0f6e392
sharon smith <sharon.e.smith2@gmail.com>
2017-02-08 22:38:40 -0600

    added styling

--- converter.js
+++ converter.js
@@ -2,6 +2,7 @@ console.log("converter is running");
 
 //*******************************************
 // define Event Listners
+//*******************************************
 var converterButton = document.getElementById("converter");
 var clearButton = document.getElementById("clear");
 var enterKey = document.getElementById("temp");
@@ -25,65 +26,6 @@ clearButton.addEventListener("click", function(){
   clearInputField();
 });
 
-function clearInputField() {
-	document.getElementById("temp").innerHTML = "";
-}
-
-
-
-//*******************************************
-// if <Enter> key or <Convert> button, 
-// determine which conversion to calculate
-function determineConverter (whichConv) {
-  // var whichConv;
-  if (document.getElementById("cels").checked) {
-  	whichConv = "toCels"; // F => C
-  } else if (document.getElementById("fahr").checked) {
-  	whichConv = "toFahr"; // C => F
-    } else {
-        alert("Please select either Fahrenheit or Celsius for conversion.");
-    }
-}
-
-//*******************************************
-// Calculate the temperature conversion
-function calcConversion (CorF) {
-  // console.log("calculating converter");
-	// var cTemp = document.getElementById("temp").innerHTML;
-	// var fTemp = document.getElementById("temp").innerHTML;
-	inputTemp = document.getElementById("temp").value;
-	if (CorF === "toFahr") {
-		// C => F :: [°F] = [°C] ×  9⁄5 + 32
-		return [ inputTemp * ( 9/5 ) ] + 32; 
-	} else { // "toCels"
-		// F => C :: [°C] = ([°F] − 32) ×  5⁄9
-		return [ ( inputTemp - 32 ) * ( 5/9 ) ];
-	}
-}
-
-//*******************************************
-// Calculate the text color for output
-// greater than 90F/32C :: RED
-// less than 32F/0C :: BLUE
-// all other :: GREEN
-function determineColor (temp, CorF) {
-	if ((CorF === "toFahr") && (temp > 90) || (CorF === "toFahr") && (temp < 32)) {
-		return "red";
-	} else if ((CorF === "toCels") && (temp > 32) || (CorF === "toCels") && (temp < 0)) {
-		return "blue";
-	} else {
-		return "green";
-	}
-}
-	
-
-//*******************************************
-// Output the result to the user
-function displayResult (result, color) {
-	console.log("displayingResult");
-	outputText.innerHTML = "<p><strong>Your Converted Temp Is: " + result + ".</strong></p>"
-	outputText.style.color = color;
-}
 
 
   
@@ -95,12 +37,91 @@ function performConversion () {
 	var convertedTemp;   // var holds the converted temp
 	var outputColor;     // var holds the color for printing the output, per specs
 
-	whichConv = determineConverter(whichConv);
+	whichConv = determineConverter();
+// console.log("from MAIN performConversion :: whichConv = ", whichConv);
 	convertedTemp = calcConversion(whichConv);
+console.log("from MAIN performConversion :: convertedTemp = ", convertedTemp);
 	outputColor = determineColor (convertedTemp);
-	displayResult(convertedTemp, outputColor);
+console.log("from MAIN performConversion :: outputColor = ", outputColor);
+	displayResult(convertedTemp, outputColor, whichConv);
 }
 
+
+
+//*******************************************
+function clearInputField() {
+	document.getElementById("temp").innerHTML = "";
+}
+
+
+//*******************************************
+// if <Enter> key or <Convert> button, 
+// determine which conversion to calculate
+//*******************************************
+function determineConverter () {
+  // var whichConv;
+  if (document.getElementById("cels").checked) {
+  	return "toCels"; // F => C
+console.log("running determineConverter :: whichConv = ", whichConv); 
+  } else if (document.getElementById("fahr").checked) {
+  	return "toFahr"; // C => F
+console.log("running determineConverter :: whichConv = ", whichConv); 
+    } else {
+        alert("Please select either Fahrenheit or Celsius for conversion.");
+    }
+}
+
+//*******************************************
+// Calculate the temperature conversion
+//*******************************************
+function calcConversion (CorF) {
+  // console.log("calculating converter");
+	// var cTemp = document.getElementById("temp").innerHTML;
+	// var fTemp = document.getElementById("temp").innerHTML;
+	inputTemp = document.getElementById("temp").value;
+	console.log("running calcConversion :: inputTemp = ", inputTemp);
+	if (CorF === "toFahr") {
+		// C => F :: [°F] = [°C] ×  9⁄5 + 32
+// console.log("running calcConversion :: CorF for toFahr = ", CorF); // CORRECT
+		return [ inputTemp * ( 9/5 ) ] + 32; 
+	} else { // "toCels"
+// console.log("running calcConversion :: CorF for toCels = ", CorF); // CORRECT
+		// F => C :: [°C] = ([°F] − 32) ×  5⁄9
+		return [ ( inputTemp - 32 ) * ( 5/9 ) ];
+	}
+}
+
+//*******************************************
+// Calculate the text color for output
+// greater than 90F/32C :: RED
+// less than 32F/0C :: BLUE
+// all other :: GREEN
+//*******************************************
+function determineColor (temp, CorF) {
+console.log("in determineColor :: temp = ", temp);
+	if ((CorF === "toFahr") && (temp > 90) || (CorF === "toFahr") && (temp < 32)) {
+console.log("in determineColor :: CorF = ", CorF);
+		return "red";
+	} else if ((CorF === "toCels") && (temp > 32) || (CorF === "toCels") && (temp < 0)) {
+		return "blue";
+	} else {
+		return "green";
+	}
+}
+	
+//*******************************************
+// Output the result to the user
+//*******************************************
+function displayResult (result, color, CorF) {
+	console.log("displayingResult");
+	outputText.style.color = color;
+	outputText.innerHTML = "<p><strong>Your Converted Temp Is: " + result + ".</strong></p>"
+	if (CorF === "toFahr") {
+		outputText.innerHTML += "&deg;F";
+	} else {
+		outputText.innerHTML += "&deg;C";
+	}
+}
 
 
 
