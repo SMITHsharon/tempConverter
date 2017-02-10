bba99636b989c71b3afab172d7a43b187de90d51
sharon smith <sharon.e.smith2@gmail.com>
2017-02-09 20:51:39 -0600

    things are looking up

--- converter.js
+++ converter.js
@@ -6,7 +6,6 @@ console.log("converter is running");
 var enterKey = document.getElementById("temp");
 var converterButton = document.getElementById("converter");
 var clearButton = document.getElementById("clear");
-var outputText = document.getElementById("output");
 
 enterKey.addEventListener("keyup", 
 	function (e) {
@@ -18,11 +17,14 @@ console.log("pressed Enter Key");
  		} 		
 });
 
+// converterButton.addEventListener("click", performConversion()); 
 converterButton.addEventListener("click", function() {
 console.log("Convert Button clicked");
 	performConversion();
 });
 
+
+// clearButton.addEventListener("click", clearInputField());
 clearButton.addEventListener("click", function() {
   // console.log("clearButton working");
   clearInputField();
@@ -37,17 +39,15 @@ clearButton.addEventListener("click", function() {
 function performConversion () {
 	var whichConv;       // reads whether (F=>C || C=>F)
 	var convertedTemp;   // var holds the converted temp
-	var getOutputColor;     // var holds the color for printing the output, per specs
-
-	whichConv = determineConverter();
-// console.log("from MAIN performConversion :: whichConv = ", whichConv); :: CORRECT
-	convertedTemp = calcConversion(whichConv);
-// console.log("from MAIN performConversion :: convertedTemp = ", convertedTemp);
-	getOutputColor = determineColor (convertedTemp);
-console.log("from MAIN performConversion :: ");
+	var getOutputColor;  // var holds the color for printing the output, per specs
+ 
+	whichConv = determineConverter(); 		   // RETURNS "toCels" or "toFahr"
+	convertedTemp = calcConversion(whichConv); // RETURNS calculated converted temp
+	theOutputColor = determineColor (convertedTemp, whichConv);
+											   // RETURNS "red", "blue", "green"
 inputTemp = document.getElementById("temp").value;
-console.log(inputTemp + " => " + convertedTemp + " " + getOutputColor + " " + whichConv);
-	displayResult(convertedTemp, getOutputColor, whichConv);
+console.log("from Main :: ", inputTemp + " => " + convertedTemp + " " + getOutputColor + " " + whichConv);
+	displayResult(convertedTemp, theOutputColor, whichConv);
 }
 
 
@@ -61,6 +61,7 @@ function clearInputField() {
 //*******************************************
 // if <Enter> key or <Convert> button, 
 // determine which conversion to calculate
+// RETURNS "toCels" or "toFahr"
 //*******************************************
 function determineConverter () {
 // var whichConv;
@@ -77,6 +78,7 @@ function determineConverter () {
 
 //*******************************************
 // Calculate the temperature conversion
+// RETURNS calculated converted temp
 //*******************************************
 function calcConversion (CorF) {
   // console.log("calculating converter");
@@ -87,11 +89,11 @@ function calcConversion (CorF) {
 	if (CorF === "toFahr") {
 		// C => F :: [°F] = [°C] ×  9⁄5 + 32
 // console.log("running calcConversion :: CorF for toFahr = ", CorF); // CORRECT
-		return [ inputTemp * ( 9/5 )  + 32 ]; 
+		return ( inputTemp * ( 9/5 )  + 32 ); 
 	} else { // "toCels"
 // console.log("running calcConversion :: CorF for toCels = ", CorF); // CORRECT
 		// F => C :: [°C] = ([°F] − 32) ×  5⁄9
-		return [ ( inputTemp - 32 ) * ( 5/9 ) ];
+		return ( ( inputTemp - 32 ) * ( 5/9 ) );
 	}
 }
 
@@ -100,15 +102,19 @@ function calcConversion (CorF) {
 // greater than 90F/32C :: RED
 // less than 32F/0C :: BLUE
 // all other :: GREEN
+// RETURNS "red", "blue", "green"
 //*******************************************
 function determineColor (temp, CorF) {
+console.log("in determineColor :: CorF :: ", CorF);
 console.log("in determineColor :: temp = ", temp);
-	if ((CorF === "toFahr") && (temp > 90) || (CorF === "toFahr") && (temp < 32)) {
-console.log("in determineColor :: CorF = ", CorF);
+	if ((CorF === "toFahr") && (temp > 90) || (CorF === "toCels") && (temp > 32)) {
+console.log("in determineColor RED :: CorF = ", CorF);
 		return "red";
-	} else if ((CorF === "toCels") && (temp > 32) || (CorF === "toCels") && (temp < 0)) {
+	} else if ((CorF === "toFahr") && (temp < 32) || (CorF === "toCels") && (temp < 0)) {
+console.log("in determineColor BLUE :: CorF = ", CorF);
 		return "blue";
 	} else {
+console.log("in determineColor GREEN :: CorF = ", CorF);
 		return "green";
 	}
 }
@@ -117,14 +123,19 @@ console.log("in determineColor :: CorF = ", CorF);
 // Output the result to the user
 //*******************************************
 function displayResult (result, color, CorF) {
-	console.log("displayingResult");
+	console.log("displayingResult", result);
+	var outputText = document.getElementById("output");
 	outputText.style.color = color;
-	outputText.innerHTML = "<p><strong>Your Converted Temp Is: " + result + ".</strong></p>"
+
+	outputText.innerHTML = "Result: " + result;
+
+	// outputText.innerHTML = "<p><strong>Your Converted Temp Is: " + result + ".</strong></p>"
 	if (CorF === "toFahr") {
 		outputText.innerHTML += "&deg;F";
 	} else {
 		outputText.innerHTML += "&deg;C";
 	}
+	console.log("this is the output :: ", outputText.innerHTML);
 }
 
 
